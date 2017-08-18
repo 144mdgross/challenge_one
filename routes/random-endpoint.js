54bbd09b2b379c3d6a713b8497dec8319518cb6b
144mdgross <144.mdgross@gmail.com>
2017-08-18 10:50:17 -0600

    changes random search query params.

--- routes/random-endpoint.js
+++ routes/random-endpoint.js
@@ -4,14 +4,14 @@ const fetch = require('node-fetch')
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 // list of words to do 'random' query to tenor gifs
-const randomSearch = ['smile', 'cat', 'panda', 'dog', 'cute', 'ostrich', 'cheese', 'bike', 'bro', 'shopping', 'sports', 'soccer', 'americans', 'elephant', 'snl', 'colbert', 'coffee', 'TV', 'hipsters']
+const randomSearch = ['smile', 'cat', 'panda', 'dog', 'cute', 'ostrich', 'cheese', 'bike', 'bro', 'shopping', 'sports', 'soccer', 'americans', 'elephant', 'coffee']
 
 router.get('/', (req, res, next) => {
   // get random element
   let randomQuery = randomSearch[Math.floor(Math.random() * randomSearch.length)]
 
 // request Tenor gifs api for random result
-  fetch(`https://api.tenor.com/v1/search?q=${randomQuery}&safesearch=moderate&key=${process.env.TENOR_API_KEY}`)
+  fetch(`https://api.tenor.com/v1/search?q=${randomQuery}&safesearch=strict&key=${process.env.TENOR_API_KEY}`)
     .then(resp => resp.json())
       .then(body => {
         let gif = body.results[0]
